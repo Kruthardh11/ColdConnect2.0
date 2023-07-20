d7abcf4f07994894c8d9b1b71fe440ecf606b523
karthik <karthiknadar1204@gmail.com>
2023-07-20 08:52:56 +0530

    teams

--- src/Components/OurTeam.jsx
+++ src/Components/OurTeam.jsx
@@ -1,74 +1,73 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import "./mid.css"
-import karthik from "../Components/assets/karthik.jpeg"
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import "./mid.css";
+import karthik from "../Components/assets/karthik.jpeg";
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const OurTeam = () => {
   return (
     <>
-    <Navbar/>
-      <div>
-        <div className="cards">
-
-        <div className="card flex flex-col items-center">
-          <div className="image my-4">
-            <img src={karthik} alt="" className="rounded-full max-w-40 max-h-40 border border-pink-300" />
-          </div>
-          <div className="text-2xl m-1">
-            Karthik Nadar
-          </div>
-          <div className="text-2xl m-1">
-            Fullstack developer
-          </div>
-          <div className="socials">
-            <div className="github">
-              <a href="https://github.com/karthiknadar1204" target='_blank' rel="noreferrer" className="hover:text-[#e5e5e5] transform hover:scale-110 transition-all">
-                <AiFillGithub/>
-              </a>
+      <Navbar />
+      <div className="min-h-screen bg-gray-100">
+        <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8">
+          {/* Add justify-center class to center the cards on desktop */}
+          <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 justify-center items-center ">
+            {/* Team Member 1 */}
+            <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center">
+              <div className="image my-4">
+                <img src={karthik} alt="" className="rounded-full max-w-40 max-h-40 border border-pink-300" />
+              </div>
+              <div className="text-2xl m-1">
+                Karthik Nadar
+              </div>
+              <div className="text-2xl m-1">
+                Fullstack developer
+              </div>
+              <div className="socials mt-4">
+                <div className="github">
+                  <a href="https://github.com/karthiknadar1204" target='_blank' rel="noreferrer" className="hover:text-gray-700 transform hover:scale-110 transition-all">
+                    <AiFillGithub />
+                  </a>
+                </div>
+                <div className="linkedin">
+                  <a href="https://www.linkedin.com/in/karthik-nadar-b2155a25b/" target='_blank' rel="noreferrer" className="hover:text-blue-500 transform hover:scale-110 transition-all">
+                    <AiFillLinkedin />
+                  </a>
+                </div>
+              </div>
             </div>
 
-            <div className="linkedin">
-              <a href="https://www.linkedin.com/in/karthik-nadar-b2155a25b/" target='_blank' rel="noreferrer" className="hover:text-[#0077B5] transform hover:scale-110 transition-all">
-                <AiFillLinkedin/>
-              </a>
+            {/* Team Member 2 */}
+            <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center mt-8 md:mt-0">
+              <div className="image my-4">
+                <img src={karthik} alt="" className="rounded-full max-w-40 max-h-40 border border-pink-300" />
+              </div>
+              <div className="text-2xl m-1">
+                Kruthardh Tirunahari
+              </div>
+              <div className="text-2xl m-1">
+                Fullstack developer
+              </div>
+              <div className="socials mt-4">
+                <div className="github">
+                  <a href="https://github.com/Kruthardh11" target='_blank' rel="noreferrer" className='hover:text-gray-700 transform hover:scale-110 transition-all'>
+                    <AiFillGithub />
+                  </a>
+                </div>
+                <div className="linkedin">
+                  <a href='https://www.linkedin.com/in/kruthardh-tirunahari-bba216264/' target='_blank' rel="noreferrer" className='hover:text-blue-500 transform hover:scale-110 transition-all'>
+                    <AiFillLinkedin />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="card flex flex-col items-center">
-          <div className="image my-4">
-            <img src={karthik} alt="" className="rounded-full max-w-40 max-h-40 border border-pink-300" />
-          </div>
-          <div className="text-2xl m-1">
-            Kruthardh Tirunahari
-          </div>
-          <div className="text-2xl m-1">
-            Fullstack developer
-          </div>
-          <div className="socials">
-          <div className="github">
-            <a href="https://github.com/Kruthardh11" target='_blank' rel="noreferrer" className='hover:text-[#e5e5e5] transform hover:scale-110 transition-all' >
-              <AiFillGithub/>
-            </a>
-          </div>
-
-          <div className="linkedin">
-            <a href='https://www.linkedin.com/in/kruthardh-tirunahari-bba216264/' target='_blank' rel="noreferrer" className='hover:text-[#0077B5] transform hover:scale-110 transition-all' >
-              <AiFillLinkedin/>
-            </a>
-          </div>
-          </div>
-        </div>
-
-
         </div>
       </div>
-      <br /><br /><br /><br /><br /><br /><br />
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default OurTeam
+export default OurTeam;
